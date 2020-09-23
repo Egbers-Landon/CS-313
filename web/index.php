cfa22b7d2a4cae94a4a8175ba35c3d5f78c039b6
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-23 14:11:42 -0600

    yo

--- web/index.php
+++ web/index.php
@@ -2,9 +2,17 @@
 <html>
 <head>
 <title>personal homepage</title>
+<style>
+body {
+  background-image: <img src="https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fill,f_auto,fl_progressive,h_533,q_auto,w_800/v1561550231/article/R16641_image1">;
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+
 </head>
+
 <body>
-<img src="https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fill,f_auto,fl_progressive,h_533,q_auto,w_800/v1561550231/article/R16641_image1" width="200" height="200">
 
 <?php
 echo "HelloWorld";
