a069c275835f5e43b6b17c7db9b06a3a7dc8ba41
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-23 16:46:42 -0600

    hi

---
+++ web/Week2/week2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="week2.css">
+<title>Week 2 Assignment</title>
+</head>
+<body>
+
+<?php
+echo "<h2>Hello World!</h2>";
+?>
+
+<p>Sup bruh?</p>
+</body>
+</html>
