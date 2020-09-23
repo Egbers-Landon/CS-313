199e126a80dc70fa49027ee44b1292536cb759f1
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-23 14:16:43 -0600

    yo

--- web/index.php
+++ web/index.php
@@ -4,7 +4,6 @@
 <title>personal homepage</title>
 <style>
 body {
-  background-image: "https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fill,f_auto,fl_progressive,h_533,q_auto,w_800/v1561550231/article/R16641_image1";
   background-repeat: no-repeat;
   background-attachment: fixed;  
   background-size: cover;
