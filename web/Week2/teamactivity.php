52e6417a168f26150b9f660572296097862c7db3
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-23 22:23:28 -0600

    hi

--- web/Week2/teamactivity.php
+++ web/Week2/teamactivity.php
@@ -6,15 +6,15 @@
 <body>
 <div>
   <div class="this" id="first">
-  <button type="button">Click Me!</button>
+  <button type="button" class="this">Click Me!</button>
   </div>
 
   <div class="this" id="second">
-  <button type="button">Click Me!</button>
+  <button type="button" class="this">Click Me!</button>
   </div>
 
   <div class="this" id="third">
-  <button type="button">Click Me!</button>
+  <button type="button" class="this">Click Me!</button>
   </div>
 </div>  
 </body>
