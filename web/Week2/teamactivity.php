fb0486a1ccd35360e3592028f55a8a106981ba03
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-23 21:55:45 -0600

    hi

---
+++ web/Week2/teamactivity.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="week2.css">
+</head>
+<body>
+<div>
+  <div class="this" id="first">
+  <button type="button">Click Me!</button>
+  </div>
+
+  <div class="this" id="second">
+  <button type="button">Click Me!</button>
+  </div>
+
+  <div class="this" id="third">
+  <button type="button">Click Me!</button>
+  </div>
+</div>  
+</body>
+</html>
