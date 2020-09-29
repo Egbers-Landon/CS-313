c9351e3f927d2678e228f3dbdd0dac6b6d397e53
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-29 16:02:40 -0600

    hi

--- web/Week3/ta3.php
+++ web/Week3/ta3.php
@@ -5,6 +5,7 @@
 Welcome <?php echo $_POST["fname"]; ?>
 <?php echo $_POST["lname"]; ?><br>
 Email: <a href="mailto:<?php echo $_POST["maile"]; ?>"><?php echo $_POST["maile"]; ?></a>
+Major: <?php echo $_POST["major"]; ?>
 
 </body>
 </html>
