4e3d500d47603519b25ac1c404bc9bdd741ce4fc
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-29 15:39:56 -0600

    hi

--- web/Week3/ta3.php
+++ web/Week3/ta3.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["fname"] ["lname"]; ?><br>
+Welcome <?php echo $_POST["fname"], ["lname"]; ?><br>
 Your email address is: <?php echo $_POST["maile"]; ?>
 
 </body>
