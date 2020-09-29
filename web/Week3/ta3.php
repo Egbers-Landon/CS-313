cc2f80cb0793f09ce883ac13cbf94ac2a11721c4
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-09-29 16:14:55 -0600

    hi

--- web/Week3/ta3.php
+++ web/Week3/ta3.php
@@ -6,6 +6,8 @@ Welcome <?php echo $_POST["fname"]; ?>
 <?php echo $_POST["lname"]; ?><br>
 Email: <a href="mailto:<?php echo $_POST["maile"]; ?>"><?php echo $_POST["maile"]; ?></a><br>
 Major: <?php echo $_POST["major"]; ?><br>
+Comments: <?php echo $_POST["comments"]; ?><br>
+Countries Visited: <?php echo $_POST["continent"]; ?><br>
 
 
 </body>
