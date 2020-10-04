83d23ceead6054ea2cb715d49fc5271112c7c58d
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-10-03 22:31:50 -0600

    hi

--- web/Week3/confirm.php
+++ web/Week3/confirm.php
@@ -1,2 +1,15 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylesheet" href="style3.css">
+    </head>
+    <body>
+        <h1>Thank you for shopping!</h1>
+        <p>
+            Your order will be shipped to <?php echo $_post["address"] ?><br>
+            <?php echo $_post["city"] ?><br>
+            <?php echo $_post["state"] ?><br>
+            <?php echo $_post["zip"] ?><br>
+        </p>
+    </body>
+</html>
