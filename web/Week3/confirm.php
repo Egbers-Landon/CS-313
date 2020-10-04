8b591349a2e4382b4d8d33a4ead2d98215984b47
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-10-03 22:38:13 -0600

    hi

--- web/Week3/confirm.php
+++ web/Week3/confirm.php
@@ -5,10 +5,10 @@
     </head>
     <body>
         <h1>Thank you for shopping!</h1>
-        <p>Your order will be shipped to: </p><?php echo $_post["address"] ?><br>
-        <?php echo $_post["city"] ?><br>
-        <?php echo $_post["state"] ?>
+        <p>Your order will be shipped to: </p><?php echo $_POST["address"] ?><br>
+        <?php echo $_POST["city"] ?><br>
+        <?php echo $_POST["state"] ?>
         <p>, </p>
-        <?php echo $_post["zip"] ?><br>
+        <?php echo $_POST["zip"] ?><br>
     </body>
 </html>
