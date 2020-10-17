6a2b62a18e3c311e3823c6ff4356c3f746c99cce
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-10-16 18:57:36 -0600

    hi

---
+++ web/Week5/mydb.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS Users CASCADE;
+CREATE TABLE Users (
+    UserID serial NOT NULL PRIMARY KEY,
+    LastName  varchar(255) NOT NULL,
+    FirstName  varchar(255) NOT NULL,
+    Address varchar(255),
+    City varchar(255),
+    Age int
+);
+
+DROP TABLE IF EXISTS Orders CASCADE;
+CREATE TABLE Orders (
+    OrderID serial NOT NULL PRIMARY KEY,
+    UserID int NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+DROP TABLE IF EXISTS Products CASCADE;
+CREATE TABLE Products (
+    ProductID serial NOT NULL PRIMARY KEY,
+    ProductName varchar(255) NOT NULL,
+    ProductPrice varchar(255) NOT NULL,
+    ProductDescription varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS Link CASCADE;
+CREATE TABLE Link (
+    LinkID serial NOT NULL PRIMARY KEY,
+    OrderID int NOT NULL REFERENCES Orders(OrderID),
+    ProductID int NOT NULL REFERENCES Products(ProductID)
+);
+
+
+INSERT INTO Users
+(LastName, FirstName, Address, City, Age)
+VALUES ('Egbers', 'Landon', '4321myhouseln', 'Clifton', '24');
+
+INSERT INTO Users
+(LastName, FirstName, Address, City, Age)
+VALUES ('Smith', 'Joseph', '1234myhouseln', 'Clifton', '23');
---
+++ web/Week5/prove.php
@@ -0,0 +1,40 @@
+<?php
+    require "../../db/dbconnect.php";
+        $db = get_db();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>Age</th>
+        </tr>
+    <?php
+        try {
+            $statement = $db->prepare('Select * FROM Users ORDERBY lastname');
+            $statement->execute();
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                $lastname = $row['lastname'];
+                $firstname = $row['firstname'];
+                $address = $row['address'];
+                $city = $row['city'];
+                $age = $row['age'];
+                echo "<tr> <td> $lastname </td> </tr>";
+            }
+            } catch (Exception $ex) {
+            echo "$ex";
+            }
+    ?>
+    </table>
+</body>
+</html>
