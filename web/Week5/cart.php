ece14ea6d83aa1023b9cc78061b40366478ad6de
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-10-23 16:09:45 -0600

    hi

--- web/Week5/cart.php
+++ web/Week5/cart.php
@@ -17,40 +17,12 @@
     <p><button style="float:right;" onclick="document.location='https://salty-lake-55999.herokuapp.com/Week5/VGS.html'">Return to Shopping</button></p><br><br>
     <table class="table table-striped">
         <tr>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>Age</th>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Product Price</th>
+            <th>Product Description</th>
         </tr>
     <?php
-        try {
-            $statement = $db->prepare('Select * FROM Users ORDER BY lastname');
-            $statement->execute();
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                $lastname = $row['lastname'];
-                $firstname = $row['firstname'];
-                $addresss = $row['address'];
-                $city = $row['city'];
-                $age = $row['age'];
-                echo "<tr> <td> $lastname </td> <td>$firstname</td> <td>$addresss</td> <td>$city</td> <td>$age</td> </tr>";
-            }
-            } catch (Exception $ex) {
-            echo "$ex";
-            }
-
-            try {
-                $statement = $db->prepare('Select * FROM Orders ORDER BY OrderID');
-                $statement->execute();
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                    $OrderID = $row['OrderID'];
-                    $UserID = $row['UserID'];
-                    echo "<tr> <td> $OrderID </td> <td>$UserID</td> </tr>";
-                }
-                } catch (Exception $ex) {
-                echo "$ex";
-                } 
-
                 try {
                     $statement = $db->prepare('Select * FROM Products ORDER BY ProductID');
                     $statement->execute();
