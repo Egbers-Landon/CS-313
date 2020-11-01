2d30ed63e3d505a53feb4cad5c3c0894c908f786
Egbers-Landon <59581938+Egbers-Landon@users.noreply.github.com>
2020-10-31 21:22:09 -0600

    hi

--- web/Week5/checkout.php
+++ web/Week5/checkout.php
@@ -6,8 +6,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="style.css>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+</head>
 </head>
     <body>
         <h1>Checking Out</h1>
